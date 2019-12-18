f5a30e4f490395fb14c77401cefe100063110984
frantuma <frantuma@yahoo.com>
2019-12-18 19:26:47 +0100

    restructure - play-2.7

--- play-2.5/swagger-play2/project/plugins.sbt
+++
@@ -1,3 +0,0 @@
-addSbtPlugin("com.typesafe.play" % "sbt-plugin"      % "2.5.4")
-
-addSbtPlugin("com.jsuereth"      % "sbt-pgp"         % "1.0.0")
--- play-2.6/swagger-play2/app/play/modules/swagger/util/CrossUtil.scala
+++
@@ -1,9 +0,0 @@
-package play.modules.swagger.util
-
-import play.routes.compiler.Parameter
-
-object CrossUtil {
-  def getParameterDefaultField(parameter: Parameter): String = {
-    parameter.default.getOrElse("")
-  }
-}
